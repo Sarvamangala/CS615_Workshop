f358bb7cd3d8bc1d71ce9c979b590f8333355330
angadi.saru <angadi.saru@gmail.com>
2018-02-22 09:21:34 -0500

    Bootstrap framework and landing page

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Feedback Box</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>Feedback Box</h1>
+  <p>Resize this responsive page to see the effect!</p> 
+</div>
+  
+<!-- new things -->
+  <form method = "post" action = "SOMETHING.php"
+        <label>Feedback:</label>
+        <textarea id="" name="" row="3" style="min-width: 100%"> </textarea>
+       
+
+</body>
+</html>
